7e5c8a27cc1116993d1995b47607b3d8a1f48dc6
Bryan Bravo <88214519+Akherox@users.noreply.github.com>
2022-07-08 19:51:35 -0300

    Finalizados todos os verbor http menos o UPDATE
    Faltou o update, não consegui pegar o id para fazer a seleção quando o DELETE sendo que antes tinha usado o mesmo metodo pra selecionar o id no GET

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { Sequelize, DataTypes } = require('sequelize')
+const { Sequelize, DataTypes, Model } = require('sequelize')
 const Task = require('./models/task')
 
 const app = express()
@@ -10,38 +10,98 @@ const tasks = Task(sequelize, DataTypes)
 app.use(express.json())
 
 // List tasks
-app.get('/tasks', (req, res) => {
-  res.json({ action: 'Listing tasks' })
+app.get('/tasks', async (req, res) => {
+  try {
+    const taskList = await tasks.findAll({})
+    res.status(200).json({ action: 'Listing task', taskList })
+  } catch (error) {
+    res.status(500).json({ error: error + ". Ups! Something wrong..." })
+  }
 })
 
 // Create task
-app.post('/tasks', (req, res) => {
-  const body = req.body
+app.post('/tasks', async (req, res, next) => {
+  const { description, done } = req.body
+  const newTask = {
+    description,
+    done
+  }
+  try {
+    await tasks.create(newTask)
 
-  res.json(body)
+    res.status(201).json({ action: 'Adding new task', newTask })
+  } catch (error) {
+    res.status(500).json({ error: error + ". Ups! Something wrong..." })
+  }
 })
 
 // Show task
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
   const taskId = req.params.id
+  try {
+    const infoTask = await tasks.findOne({ where: { id: taskId } })
 
-  res.send({ action: 'Showing task', taskId: taskId })
+    if (!infoTask) {
+      res.status(422).json({ message: 'This task does not exists!' })
+      return
+    }
+
+    res.status(200).send({
+      action: 'Showing task',
+      infoTask
+    })
+  } catch (error) {
+    res.status(500).json({ error: error + ". Ups! Something wrong..." })
+  }
 })
 
-// Update task
-app.put('/tasks/:id', (req, res) => {
+// Update task --------------------------- FALTA ESTO NADA MAS (UPDATE) ------------------------
+app.put('/tasks/:id', async (req, res) => {
   const taskId = req.params.id
+  const { description, done } = req.body
+  const task = {
+    description,
+    done
+  }
+  try {
+    const updatedTask = await tasks.update({ where: { id: taskId }, task })
+    //No consegui tomar el id y eso que anteriormente lo consegui usando este metodo (falta tomar el id para hacer la seleccion de la task que va a ser eliminada)
 
+    if(updatedTask.matchedCount === 0) {
+      res.status(422).json({ message: 'This task does not exists!' })
+      return
+    }
+
+    res.status(200).send({
+      action: 'Updating task',
+      updatedTask
+    })
+  } catch (error) {
+    res.status(500).json({ error: error + ". Ups! Something wrong..." })
+  }
   res.send({ action: 'Updating task', taskId: taskId })
 })
 
 // Delete task
-app.delete('/tasks/:id', (req, res) => {
+app.delete('/tasks/:id', async (req, res) => {
   const taskId = req.params.id
+  try {
+    const taskToDelete = await tasks.destroy({ where: { id: taskId } })
 
-  res.send({ action: 'Deleting task', taskId: taskId })
+    if (!taskToDelete) {
+      res.status(422).json({ message: 'This task does not exists!' })
+      return
+    }
+
+    res.status(200).send({
+      action: 'Deleting task',
+      deletedTaskId: taskId
+    })
+  } catch (error) {
+    res.status(500).json({ error: error + ". Ups! Something wrong..." })
+  }
 })
 
 app.listen(3000, () => {
-  console.log('Iniciando o ExpressJS na porta 3000')
+  console.log('Iniciado o ExpressJS na porta 3000')
 })
